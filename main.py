e08d89e6eabed4b3d160ace49e3e6c856477f14e
mikechess <mikecook450@gmail.com>
2025-10-07 15:38:31 -0400

    deposite function

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 # Python Banking Program
 
 def show_balance():
-    pass
+    print(f"Your current balance is: ${balance:.2f}")
 
 
-def deposite():
-    pass
+def deposit():
+    amount = float(input("Enter amount to be deposited: $"))
+    
+    if amount < 0:
+        print("That's not a valid amount.")
+        return 0
+    else:
+        return amount
 
 
 def withdraw():
@@ -18,7 +24,7 @@ is_running = True
 while is_running:
     print("Banking Program")
     print("1. Show Balance")
-    print("2. Deposite")
+    print("2. Deposit")
     print("3. Withdraw")
     print("4. Exit")
     
@@ -27,7 +33,7 @@ while is_running:
     if choice == '1':
         show_balance()
     elif choice == '2':
-        deposite()
+        balance += deposit()
     elif choice == '3':
         withdraw()
     elif choice == '4':
